16fed017030080a4760a4d03cb4b8b81f85e8047
dadabit <dcarboni@gmail.com>
2018-01-09 15:55:26 +0100

    infura conf

--- truffle.js
+++ truffle.js
@@ -1,3 +1,18 @@
+var provider, address;
+
+
+
+var providerURL = `https://ropsten.infura.io/ROeiriNDGwCQC0OOochi `
+var HDWalletProvider = require('truffle-hdwallet-provider');
+// todo: Think about more secure way
+var mnemonic = "to be or not to be that is the question";
+provider = new HDWalletProvider(mnemonic, providerURL, 0);
+address = "0x" + provider.wallet.getAddress().toString("hex");
+console.log('Provider address', provider.getAddress());
+console.log('Deploying to ', providerURL);
+
+
+
 module.exports = {
   networks: {
     development: {
@@ -6,13 +21,13 @@ module.exports = {
       network_id: "*" // Match any network id
     },
    ropsten: {
-    host: "127.0.0.1", // localhost, have your geth ready
-    port: 8545,
+    //host: "127.0.0.1", // localhost, have your geth ready
+    //port: 8545,
     network_id: 3,        // Ethereum test network
     gas: 4500000,
     gasPrice: 22000000000, //yeah 22Gwei
-    // from - default address to use for any transaction Truffle makes during migrations
-    // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+    from: address,// - default address to use for any transaction Truffle makes during migrations
+    provider: provider // - web3 provider instance Truffle should use to talk to the Ethereum network.
     //          - if specified, host and port are ignored.
    }
   }
